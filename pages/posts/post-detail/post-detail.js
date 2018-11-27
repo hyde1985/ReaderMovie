9ea6398e07f62d90b3ae6d799695387942ede22c
Hyde Sun <hyde_sun@sina.com>
2018-11-27 23:15:22 +0800

    code 6-4

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -4,6 +4,9 @@ var postsData = require('../../../data/post-data.js')
 Page({
   onLoad:function(option) {
     var postId = option.id
-
+    var postData = postsData.postList[postId]
+    this.setData({
+      postData: postData
+    })
   }
 })
