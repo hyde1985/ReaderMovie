7ca729a1d77519af9b93557117592ab0a09133b8
Hyde Sun <hyde_sun@sina.com>
2018-12-01 00:27:17 +0800

    code 6-7

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -2,11 +2,35 @@ var postsData = require('../../../data/post-data.js')
 
 
 Page({
+  data: {
+
+  },
   onLoad:function(option) {
     var postId = option.id
+    this.data.currentPostId = postId
     var postData = postsData.postList[postId]
     this.setData({
       postData: postData
     })
+    var postsCollected = wx.getStorageSync("post_collected")
+    if(postsCollected) {
+      var postCollected = postsCollected[postId] 
+      this.setData({
+        collected: postCollected
+      })
+    } else {
+      var postsCollected = {}
+      postsCollected[postId] = false
+      wx.setStorageSync("post_collected", postsCollected)
+    }
+    
+  },
+
+  onCollectionTap: function(event) {
+    var postsCollected = wx.getStorageSync("post_collected")
+    var postCollected = postsCollected[this.data.currentPostId]
+    postCollected = !postCollected
+    postsCollected[this.data.currentPostId] = postCollected
+    wx.setStorageSync("post_collected", postsCollected)
   }
 })
